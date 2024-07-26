2dbe5b7ed8566bb45b64ab07af7233b865b33f2e
Carlos Diez <cdiezmoran@gmail.com>
2024-07-26 11:40:47 -0600

    Create pyproject.toml

--- setup.py
+++ setup.py
@@ -2,26 +2,15 @@ from setuptools import setup, find_packages
 
 # Function to read the requirements.txt file
 def parse_requirements(filename):
-    requirements = []
-    dependency_links = []
     with open(filename, 'r') as file:
-        for line in file:
-            line = line.strip()
-            if line.startswith('--'):
-                dependency_links.append(line)
-            else:
-                requirements.append(line)
-    return requirements, dependency_links
-
-requirements, dependency_links = parse_requirements('requirements.txt')
+        return file.read().splitlines()
 
 setup(
     name='mvdiffusion',
     version='0.1.0',
     description='',
-    url='https://github.com/Mirage-AI/mvdiffusion',
+    url='https://github.com/Mirage-AI/mvdiffusion',  # Update with your project's URL
     packages=find_packages(),
-    install_requires=requirements,
-    dependency_links=dependency_links,
+    install_requires=parse_requirements('requirements.txt'),
     python_requires='>=3.6',
 )
