4fa9584ce73cfcb3c7b60c0b44950f0889dde721
Carlos Diez <cdiezmoran@gmail.com>
2024-07-26 11:52:20 -0600

    Revert pyproject

--- setup.py
+++ setup.py
@@ -2,15 +2,26 @@ from setuptools import setup, find_packages
 
 # Function to read the requirements.txt file
 def parse_requirements(filename):
+    requirements = []
+    dependency_links = []
     with open(filename, 'r') as file:
-        return file.read().splitlines()
+        for line in file:
+            line = line.strip()
+            if line.startswith('--'):
+                dependency_links.append(line)
+            else:
+                requirements.append(line)
+    return requirements, dependency_links
+
+requirements, dependency_links = parse_requirements('requirements.txt')
 
 setup(
     name='mvdiffusion',
     version='0.1.0',
     description='',
-    url='https://github.com/Mirage-AI/mvdiffusion',  # Update with your project's URL
+    url='https://github.com/Mirage-AI/mvdiffusion',
     packages=find_packages(),
-    install_requires=parse_requirements('requirements.txt'),
+    install_requires=requirements,
+    dependency_links=dependency_links,
     python_requires='>=3.6',
 )
